51ea83808369ae47c443b1bd763a160fb0846571
Bas Biezemans <basbiezemans@users.noreply.github.com>
2024-07-21 13:23:43 +0000

    Tests

--- src/main.rs
+++ src/main.rs
@@ -58,3 +58,23 @@ mod user_input {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn code_from_correct_string() {
+        assert!(code::from_string("1234".to_string()).is_some());
+        assert!(code::from_string("6543".to_string()).is_some());
+    }
+
+    #[test]
+    fn code_from_incorrect_string() {
+        assert!(code::from_string("123".to_string()).is_none());
+        assert!(code::from_string("12345".to_string()).is_none());
+        assert!(code::from_string("1237".to_string()).is_none());
+        assert!(code::from_string("123x".to_string()).is_none());
+        assert!(code::from_string("0123".to_string()).is_none());
+    }
+}
