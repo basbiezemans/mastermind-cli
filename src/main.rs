3971ed25938c0b432a2dac55bb129067282b14f8
Bas Biezemans <basbiezemans@users.noreply.github.com>
2024-07-21 12:04:03 +0000

    User input and error logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use log::error;
+use user_input::get_input;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -14,5 +16,23 @@ fn main() {
     println!("| Mastermind, the code-breaking game |");
     println!("+------------------------------------+");
     println!("You have {} turns to guess the code. Good luck!", limit);
-    println!("Guess:");
+
+    match get_input("Guess: ") {
+        Ok(guess) => println!("You guessed {}", guess),
+        Err(error) => error!("Input error: {}", error),
+    }
+}
+
+mod user_input {
+    use std::io::stdin;
+    use std::io::Write;
+    pub fn get_input(prompt: &str) -> Result<String, std::io::Error> {
+        print!("{}", prompt);
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        match stdin().read_line(&mut input) {
+            Err(e) => Err(e),
+            Ok(_) => Ok(input.trim().to_string()),
+        }
+    }
 }
