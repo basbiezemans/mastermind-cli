15f2ccd3250dc720b1c5acd8e0cfa63c657d14b3
Bas Biezemans <basbiezemans@users.noreply.github.com>
2024-07-21 10:06:34 +0000

    Number of turns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
+    let limit: u8 = 10;
     println!("+------------------------------------+");
     println!("| Mastermind, the code-breaking game |");
     println!("+------------------------------------+");
+    println!("You have {} turns to guess the code. Good luck!", limit);
+    println!("Guess:");
 }
