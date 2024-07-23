4124828362e35558ee3fcfdf9a9bd8e1b7c18751
Bas Biezemans <bas.biezemans@gmail.com>
2024-07-23 11:05:15 +0100

    Random secret

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use log::error;
 use mastermind_cli::code;
 use mastermind_cli::user_input::get_input;
+use rand::distributions::{Distribution, Uniform};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,6 +14,7 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let limit: u8 = args.turns;
+    let secret: String = make_secret();
     println!("+------------------------------------+");
     println!("| Mastermind, the code-breaking game |");
     println!("+------------------------------------+");
@@ -32,3 +34,16 @@ fn main() {
         Err(error) => error!("Input error: {}", error),
     }
 }
+
+fn make_secret() -> String {
+    const DIGITS: &[u8] = b"123456";
+    const LENGTH: usize = 4;
+    let mut rng = rand::thread_rng();
+    let dist = Uniform::from(0..6);
+    let rand = |_| {
+        let idx = dist.sample(&mut rng);
+        DIGITS[idx] as char
+    };
+    (0..LENGTH).map(rand).collect()
+}
+
